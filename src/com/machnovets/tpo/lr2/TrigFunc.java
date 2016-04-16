d55eaa63ac82f0bba31b2b97e40e4c4449faf9cb
larechik <lerunchicm@gmail.com>
2016-04-17 01:07:48 +0300

    Не работает
    Хз почему не работает

--- src/com/machnovets/tpo/lr2/TrigFunc.java
+++ src/com/machnovets/tpo/lr2/TrigFunc.java
@@ -1,21 +1,55 @@
 package com.machnovets.tpo.lr2;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 /**
  * Created by Makhnovets on 15.04.2016.
  */
 public class TrigFunc {
-    double res;
-    public double sin(double x, double accurancy){
-        res = 0;
-        int buf;
-        if (x>Math.PI){
-            buf=(int)Math.floor(x/Math.PI);
-            res=x-buf*Math.PI;
+    public double sin(double x, double accuracy){
+        boolean negative;
+        Double buf;
+        Double y=0.0;
+        int n=0;
+        if(x<-Math.PI||x>Math.PI){
+            buf=x/(2*Math.PI);
+            if (x%(2*Math.PI)==0){
+                x-=2*Math.PI*buf.intValue();
+            }
+            x-=2*Math.PI*(buf.intValue()+1);
         }
-        if (x<-Math.PI) {
-            buf = (int) Math.rint(x / Math.PI);
-            res = x - buf * Math.PI;
+        if (x>=-Math.PI&&x<0){
+            negative=true;
+            x=Math.abs(x);
         }
-            return res;
+        else
+        {
+            negative=false;
+        }
+        while (true){
+            BigDecimal tmpBig=BigDecimal.valueOf(Math.pow(-1,n));
+            tmpBig.multiply(BigDecimal.valueOf(Math.pow(x,2*n+1)));
+            tmpBig.divide(factorial(2*n+1),BigDecimal.valueOf(accuracy).scale(),RoundingMode.HALF_UP);
+            Double tmpDub=Math.abs(tmpBig.doubleValue());
+            if (tmpDub>accuracy)
+                y+=tmpDub;
+            else
+                break;
+            n++;
+        }
+        if (negative)
+            return -y;
+        else
+            return y;
+
+    }
+
+    private BigDecimal factorial(int num) {
+        BigDecimal fact = BigDecimal.valueOf(1);
+
+        for (; num > 0; fact = fact.multiply(BigDecimal.valueOf(num--))) ;
+
+        return fact;
     }
 }
--- test/TrigFuncTest.java
+++ test/TrigFuncTest.java
@@ -2,6 +2,7 @@
 
 import com.machnovets.tpo.lr2.TrigFunc;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 //import static junit.framework.Assert.*;
@@ -10,12 +11,116 @@ import org.junit.Test;
  * Created by Makhnovets on 15.04.2016.
  */
 public class TrigFuncTest {
+    TrigFunc func=new TrigFunc();
+    double accurancy =0.001;
+    double res;
+    @Before
+    public void Before(){
+        res=0;
+    }
+
     @Test
-    public void SinTest(){
-        TrigFunc x=new TrigFunc();
-        double a=x.sin(-7,1);
-        Assert.assertTrue(a>-Math.PI);
-        Assert.assertTrue(a<0);
+    public void SinTestPoint0(){
+        double sin=0;
+        res=func.sin(0,accurancy);
+        Assert.assertEquals("error: sin=" +sin+" res="+res,sin,res,0.01);
+    }
+    @Test
+    public void SinTestPoint0_r(){
+        double sin=0.0299955;
+        res=func.sin(0.03,accurancy);
+        Assert.assertEquals("error: sin=" +sin+" res="+res,sin,res,0.01);
+    }
+    @Test
+    public void SinTestPoint0_l(){
+        double sin=-0.0299955;
+        res=func.sin(-0.03,accurancy);
+        Assert.assertEquals("error: sin=" +sin+" res="+res,sin,res,0.01);
+    }
+    @Test
+    public void SinTestPointPiDiv2(){
+        double sin=1;
+        res=func.sin(Math.PI/2,accurancy);
+        Assert.assertEquals("error: sin=" +sin+" res="+res,sin,res,0.01);
+    }
+    @Test
+    public void SinTestPointPiDiv2_r(){
+        double sin=0.999550034;
+        res=func.sin(Math.PI/2+0.03,accurancy);
+        Assert.assertEquals("error: sin=" +sin+" res="+res,sin,res,0.01);
+    }
+    @Test
+    public void SinTestPointPiDiv2_l(){
+        double sin=0.999550034;
+        res=func.sin(Math.PI/2-0.03,accurancy);
+        Assert.assertEquals("error: sin=" +sin+" res="+res,sin,res,0.01);
+    }
+    @Test
+    public void SinTestPointMPiDiv2(){
+        double sin=-1;
+        res=func.sin(Math.PI/2,accurancy);
+        Assert.assertEquals("error: sin=" +sin+" res="+res,sin,res,0.01);
+    }
+    @Test
+    public void SinTestPointMPiDiv2_r(){
+        double sin=-0.999550034;
+        res=func.sin(-Math.PI/2+0.03,accurancy);
+        Assert.assertEquals("error: sin=" +sin+" res="+res,sin,res,0.01);
+    }
+    @Test
+    public void SinTestPointMPiDiv2_l(){
+        double sin=-0.999550034;
+        res=func.sin(-Math.PI/2-0.03,accurancy);
+        Assert.assertEquals("error: sin=" +sin+" res="+res,sin,res,0.01);
+    }
+    @Test
+    public void SinTestPointPi(){
+        double sin=0;
+        res=func.sin(Math.PI,accurancy);
+        System.out.println(sin+" "+res);
+        Assert.assertEquals("error: sin=" +sin+" res="+res,sin,res,0.01);
+    }
+    @Test
+    public void SinTestPointPi_r(){
+        double sin=-0.0299955;
+        res=func.sin(Math.PI+0.03,accurancy);
+        Assert.assertEquals("error: sin=" +sin+" res="+res,sin,res,0.01);
+    }
+    @Test
+    public void SinTestPointPi_l(){
+        double sin=0.0299955;
+        res=func.sin(Math.PI-0.03,accurancy);
+        Assert.assertEquals("error: sin=" +sin+" res="+res,sin,res,0.01);
+    }
+    @Test
+    public void SinTestPointMPi(){
+        double sin=0;
+        res=func.sin(-Math.PI,accurancy);
+        Assert.assertEquals("error: sin=" +sin+" res="+res,sin,res,0.01);
+    }
+    @Test
+    public void SinTestPointMPi_r(){
+        double sin=-0.0299955;
+        res=func.sin(-Math.PI+0.03,accurancy);
+        Assert.assertEquals("error: sin=" +sin+" res="+res,sin,res,0.01);
+    }
+    @Test
+    public void SinTestPointMPi_l(){
+        double sin=0.0299955;
+        res=func.sin(-Math.PI-0.03,accurancy);
+        Assert.assertEquals("error: sin=" +sin+" res="+res,sin,res,0.01);
+    }
+    @Test
+    public void SinTesrPosInf(){
+        double sin=Double.NaN;
+        res=func.sin(Double.POSITIVE_INFINITY,accurancy);
+        Assert.assertEquals("error: sin=" +sin+" res="+res,sin,res,0.01);
+    }
+    @Test
+    public void SinTesrNegInf(){
+        double sin=Double.NaN;
+        res=func.sin(Double.NEGATIVE_INFINITY,accurancy);
+        Assert.assertEquals("error: sin=" +sin+" res="+res,sin,res,0.01);
     }
 
 
