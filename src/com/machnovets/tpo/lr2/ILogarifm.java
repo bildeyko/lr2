7d2142649c2a6b46a9b6fe265ff94fe7a5169391
larechik <lerunchicm@gmail.com>
2016-04-17 20:21:07 +0300

    ILogarifm

--- src/com/machnovets/tpo/lr2/ILogarifm.java
+++ src/com/machnovets/tpo/lr2/ILogarifm.java
@@ -3,7 +3,7 @@ package com.machnovets.tpo.lr2;
 /**
  * Created by Makhnovets on 17.04.2016.
  */
-public interface ILn {
+public interface ILogarifm {
     double ln(double x,double accuracy);
     double log5(double x,double accuracy);
     double log5pow2(double x,double accuracy);
